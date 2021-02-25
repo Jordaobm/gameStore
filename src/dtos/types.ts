c82bfea4593fa31b770300f74adb1a16280daa57
Jordão Beghetto Massariol <jbmassariol@gmail.com>
2021-02-24 23:04:39 -0300

    removing use of the api and directly importing product.json

--- src/dtos/types.ts
+++ src/dtos/types.ts
@@ -1,38 +1,33 @@
 export interface Product {
-    id: number;
-    name: string;
-    price: number;
-    score: number;
-    image: string;
-    banner:string;
-    developer_name:string;
-    categories:string;
-    ParentalRating:string;
-    Description:string;
+  id: number;
+  name: string;
+  price: number;
+  score: number;
+  image: string;
+  banner: string;
+  developer_name: string;
+  categories: string;
+  ParentalRating: string;
+  Description: string;
 }
 
 export interface ProductCart {
-    quantity:number;
-    product:Product;
+  quantity: number;
+  product: Product;
 }
 
 export interface Cart {
-    OrderTotal:number;
-    OrderSubtotal:number;
-    FreteTotal:number;
-    products:ProductCart[];
+  OrderTotal: number;
+  OrderSubtotal: number;
+  FreteTotal: number;
+  products: ProductCart[];
 }
 
-
-
-
-
-
 export interface LocalStorageProductCart {
-    quantity:number;
-    product:number;
+  quantity: number;
+  product: number;
 }
 
 export interface LocalStorageCart {
-    products:LocalStorageProductCart[];
+  products: LocalStorageProductCart[];
 }
